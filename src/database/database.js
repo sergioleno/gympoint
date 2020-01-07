aafba1c15938038b06895795ab2f5fcdf3d7b32d
sergioleno <sergioleno@gmail.com>
2020-01-07 13:39:27 -0300

    commit 2 antes de gerar seed

---
+++ src/database/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'gympoint',
+  password: 'gympoint',
+  database: 'gympoint',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
